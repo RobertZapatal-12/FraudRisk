d155ec7961a21ade6999ba333a1446925773a857
RobertZapatal-12 <yarelzapatal@gmail.com>
2026-08-16 02:57:26 -0400

    Implement logistic regression workflow and add runall module

--- app/main.py
+++ app/main.py
@@ -1,16 +1,5 @@
-from src.preprocessing.preprocess import preprocess
-from src.training.training import training, save_model 
-from src.evaluation.evaluate import evaluate_model
-import pandas as pd
+from runall.runall_models import run_logistic_regresion
 
-df = pd.read_csv("data/processed/creditcard_clean.csv")
 
-x_train_scaled, x_test_scaled, y_train, y_test = preprocess(df)
+run_logistic_regresion()
 
-model_lr = training(x_train_scaled, y_train)
-
-evalu = evaluate_model(model_lr)
-
-save_model(model_lr)
-
-print(evalu)
---
+++ runall/runall_models.py
@@ -0,0 +1,21 @@
+from src.preprocessing.preprocess import preprocess
+from src.training.training import training, save_model 
+from src.evaluation.evaluate import evaluate_model
+import pandas as pd
+
+def run_logistic_regresion():
+
+    df = pd.read_csv("data/processed/creditcard_clean.csv")
+
+    x_train_scaled, x_test_scaled, y_train, y_test = preprocess(df)
+
+    model_lr = training(x_train_scaled, y_train)
+
+    evalu = evaluate_model(model_lr)
+
+    save_model(model_lr)
+
+    print("=" *25)
+    print("Logistics Regression")
+    print("=" *25)
+    print(evalu)
